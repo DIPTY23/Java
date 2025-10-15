03d97db58328753a0b59ca984f883e5738d52dda
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-10-15 23:36:11 +0530

    Add files via upload

---
+++ Example5.java
@@ -0,0 +1,9 @@
+class Example5 {
+	public static void main(String args[]) {
+		System.out.println(20+20);          //20
+		System.out.println(20+20 + " is the number");      //1010
+		System.out.println("The number is "+20+20);        //1010
+		  
+	}
+
+}
