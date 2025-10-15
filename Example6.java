3c3b3806a3c463fbf716779a5968a3a8a033217f
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-10-15 23:39:34 +0530

    Delete Example6.java

--- Example6.java
+++
@@ -1,9 +0,0 @@
-class Example6 {
-	public static void main(String args[]) {
-		System.out.println(20+20);          //20
-		System.out.println(20+20 + " is the number");      //1010
-		System.out.println("The number is "+(20+20));        //1010
-		  
-	}
-
-}
