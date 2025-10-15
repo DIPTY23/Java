f386e49b60722390e44c7021a9438ac9f6b87a71
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-10-16 00:04:53 +0530

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,9 @@
+class example2 {
+	public static void main(String args[]) {
+		System.out.println("Jspiders" + "Java");
+		System.out.println("Boys" + 90);
+		System.out.println(143 + "Students");
+
+	}
+
+}
