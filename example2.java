11fac8ae6f3c77b8f07cc04e49f60fc137e4c66a
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-10-21 12:10:35 +0530

    Delete example2.java

--- example2.java
+++
@@ -1,9 +0,0 @@
-class example2 {
-	public static void main(String args[]) {
-		System.out.println("Jspiders" + "Java");
-		System.out.println("Boys" + 90);
-		System.out.println(143 + "Students");
-
-	}
-
-}
