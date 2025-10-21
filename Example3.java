1d02268484cd81113ebb05cbad0de303b152c94c
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-10-21 12:09:04 +0530

    Delete Example3.java

--- Example3.java
+++
@@ -1,7 +0,0 @@
-class Example3 {
-	public static void main(String args[]) {
-		System.out.println("Core Java" + " Programming");
-		System.out.println("SQL - Database");
-		System.out.println("Web-Technology: " + "HTML,CSS,JS");
-	}
-}
